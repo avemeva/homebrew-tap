ca9b5aed4a9f28f64308cec56c9c0b082a9303cf
Andrey <andrew.shvv@gmail.com>
2026-03-06 17:31:11 +0300

    Add tg formula v0.1.0 (darwin-arm64 only)

---
+++ Formula/tg.rb
@@ -0,0 +1,46 @@
+# typed: false
+# frozen_string_literal: true
+
+class Tg < Formula
+  desc "AI-powered Telegram CLI"
+  homepage "https://github.com/avemeva/kurier"
+  version "0.1.0"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/avemeva/kurier/releases/download/v0.1.0/tg-darwin-arm64.zip"
+      sha256 "7ccc1fce42b151abe8b55978144dad566e244c63eeb62350ca00170c6d723572"
+
+      def install
+        bin.install "tg"
+      end
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/avemeva/kurier/releases/download/v0.1.0/tg-darwin-x64.zip"
+      sha256 "MISSING"
+
+      def install
+        bin.install "tg"
+      end
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.intel? and Hardware::CPU.is_64_bit?
+      url "https://github.com/avemeva/kurier/releases/download/v0.1.0/tg-linux-x64.tar.gz"
+      sha256 "MISSING"
+
+      def install
+        bin.install "tg"
+      end
+    end
+    if Hardware::CPU.arm? and Hardware::CPU.is_64_bit?
+      url "https://github.com/avemeva/kurier/releases/download/v0.1.0/tg-linux-arm64.tar.gz"
+      sha256 "MISSING"
+
+      def install
+        bin.install "tg"
+      end
+    end
+  end
+end
